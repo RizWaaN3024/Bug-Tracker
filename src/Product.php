8149a79645100acb88d0985027b3a4efdf20b0e1
RizWaaN3024 <rizwaanans786@gmail.com>
2025-01-08 01:55:15 +0530

    Added script to insert products

--- src/Product.php
+++ src/Product.php
@@ -12,4 +12,19 @@ class Product
     private int|null $id = null;
     #[ORM\Column(type: 'string')]
     private string $name;
+
+    public function getId() :int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
 }
