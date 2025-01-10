757b4c3b75046528032a8e3db0c75fb65d7c8cf2
RizWaaN3024 <rizwaanans786@gmail.com>
2025-01-11 00:21:56 +0530

    Added Bug entity

--- src/Bug.php
+++ src/Bug.php
@@ -19,4 +19,39 @@ class Bug
 
     #[ORM\Column(type: 'string')]
     private string $status;
+
+    public function getId(): int|null
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function setCreated(DateTime $created)
+    {
+        $this->created = $created;
+    }
+
+    public function getCreated(): DateTime
+    {
+        return $this->created;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
 }
