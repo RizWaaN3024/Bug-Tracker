63e74cca0ddef1bc05cac90940bf00ae4d0d09f4
RizWaaN3024 <rizwaanans786@gmail.com>
2025-01-11 00:27:53 +0530

    Added User entity

---
+++ src/TestUser.php
@@ -0,0 +1,47 @@
+<?php
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'user')]
+class TestUser 
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private int|null $id = null;
+    #[ORM\Column(type: 'string')]
+    private string $username;
+    private $passwordHash;
+    private $bans;
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getPasswordHash(): string
+    {
+        return $this->passwordHash;
+    }
+
+    public function setPasswordHash(string $passwordHash): void
+    {
+        $this->passwordHash = $passwordHash;
+    }
+
+    public function getBans(): array
+    {
+        return $this->bans;
+    }
+
+    public function addBan(Ban $bans): void
+    {
+        $this->bans[] = $bans;
+    }
+}
--- src/User.php
+++ src/User.php
@@ -4,44 +4,13 @@ use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'user')]
-class User 
+class User
 {
     #[ORM\Id]
-    #[ORM\Column(type: 'integer')]
     #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
     private int|null $id = null;
+
     #[ORM\Column(type: 'string')]
-    private string $username;
-    private $passwordHash;
-    private $bans;
-
-    public function getUsername(): string
-    {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): void
-    {
-        $this->username = $username;
-    }
-
-    public function getPasswordHash(): string
-    {
-        return $this->passwordHash;
-    }
-
-    public function setPasswordHash(string $passwordHash): void
-    {
-        $this->passwordHash = $passwordHash;
-    }
-
-    public function getBans(): array
-    {
-        return $this->bans;
-    }
-
-    public function addBan(Ban $bans): void
-    {
-        $this->bans[] = $bans;
-    }
+    private string $name;
 }
