734c5dd39977c09abed2fde09e8d8ead237ad28b
RizWaaN3024 <rizwaanans786@gmail.com>
2025-01-11 11:56:57 +0530

    Updated User entity

--- src/User.php
+++ src/User.php
@@ -13,4 +13,19 @@ class User
 
     #[ORM\Column(type: 'string')]
     private string $name;
+
+    public function getId(): int|null
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
 }
